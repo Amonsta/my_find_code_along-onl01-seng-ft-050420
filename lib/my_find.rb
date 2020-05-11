e46086d43081b7c4d65790016383d50b521e3a1f
Amana Ammishaddai <amana.ammi@gmail.com>
2020-05-11 17:49:32 +0000

    Automatically backed up by Learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -2,11 +2,11 @@ require 'pry'
 
 def my_find(collection)
   i = 0
-  new_array = []
+  
   while i < collection.length
-  new_array << yield(collection[i])
+  yield
   i += 1
   end
-new_array
+
 end
 
